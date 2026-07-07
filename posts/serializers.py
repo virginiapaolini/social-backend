1c00f6b0861c65175abb274447d587d11b6c1724
virginiapaolini <virginia.paolini@edu.unifi.it>
2026-07-07 10:01:57 +0200

    improved serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,13 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'post', 'author', 'content', 'created_at']
         read_only_fields = ['id', 'post', 'author', 'created_at']
 
+    # evita commenti vuoti
+    def validate_content(self, value):
+        if not value.strip():
+            raise serializers.ValidationError(
+                "Il contenuto del commento non può essere vuoto o contenere solo spazi.")
+        return value
+
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     likes_count = serializers.IntegerField(source='likes.count', read_only=True)
@@ -21,3 +28,10 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'author', 'content', 'created_at', 'updated_at', 'likes_count', 'comments']
         read_only_fields = ['id', 'author', 'created_at', 'updated_at']
+
+   # lunghezza minima dei post
+    def validate_content(self, value):
+        clean_value = value.strip()
+        if len(clean_value) < 5:
+            raise serializers.ValidationError("Il contenuto del post deve essere lungo almeno 5 caratteri.")
+        return clean_value
