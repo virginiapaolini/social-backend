0220de8ebec439721af6fccb2f982905a6f01447
virginiapaolini <virginia.paolini@edu.unifi.it>
2026-07-07 15:30:43 +0200

    Configurato WhiteNoise per fixare la grafica di DRF su Render

--- socialAPI/settings.py
+++ socialAPI/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#  gestione su Render
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# per compressione e il caching dei file grafici di DRF
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
